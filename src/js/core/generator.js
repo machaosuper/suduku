4070a585b54f16ff972394b531681fdfc3232985
chao.ma <chao.ma@lincomb.com>
2018-01-19 18:03:14 +0800

    ''

--- src/js/core/generator.js
+++ src/js/core/generator.js
@@ -6,6 +6,9 @@ class Generator {
     generate () {
 
         this.matrix = Toolkit.matrix.makeMatrix();
+        this.orders = Toolkit.matrix.makeMatrix()
+            .map(row => row.map((v, i) => i))
+            .map(row => Toolkit.matrix.shuffle(row));
 
         for (let n = 1; n <= 9; n++) {
             this.fillNumber(n);
@@ -23,15 +26,18 @@ class Generator {
         }
 
         const row = this.matrix[rowIndex];
+        // 随机选着列
+        const orders = this.orders[rowIndex];
+        debugger;
         for (let i; i < 9; i++) {
-            const colIndex = i;
+            const colIndex = orders[i];
             // 如果这个位置已经有值，跳过
             if (row[colIndex]) {
                 continue;
             }
 
             // 检查这个位置是否可以填 n
-            if (!Toolkit.matrix.checkFillable()) {
+            if (!Toolkit.matrix.checkFillable(this.matrix, n, rowIndex, colIndex)) {
                 continue;                
             }
 
@@ -49,3 +55,7 @@ class Generator {
         
     }
 }
+
+const generator = new Generator();
+generator.generate()
+console.log(generator.matrix)
--- src/js/core/toolkit.js
+++ src/js/core/toolkit.js
@@ -41,8 +41,15 @@ const matrixToolkit = {
     /**
      * 检测指定位置是否可以填写数字 n
      */
-    checkFillable () {
-        return ture;
+    checkFillable (matrix, n, rowIndex, colIndex) {
+        const row = matrix[rowIndex];
+        const column = this.makeRow().map((v,i) => matrix[i][colIndex]);
+        const { boxIndex } = boxTookit.convertToBoxIndex(rowIndex, colIndex);
+        const box = boxTookit.getBoxCells(matrix, boxIndex);
+        for (let i = 0; i < 9; i++) {
+            if (row[i] === n || column[i] === n || box[i] === n) return false;
+        }
+        return true;
     }
 
 };
@@ -51,8 +58,32 @@ const matrixToolkit = {
  * 宫坐标系工具
  */
 
-const boxToolit = {
+const boxTookit = {
+    convertToBoxIndex (rowIndex, colIndex) {
+        return {
+            boxIndex: Math.floor(rowIndex / 3) * 3 + Math.floor(colIndex / 3),
+            cellIndex: rowIndex % 3 * 3 + colIndex % 3
+        }
+    },
 
+    convertFromBoxIndex (boxIndex, cellIndex) {
+        return {
+            rowIndex: Math.floor(boxIndex / 3) * 3 + Math.floor(cellIndex / 3),
+            colIndex: boxIndex % 3 * 3 + cellIndex % 3
+        }
+    },
+
+    getBoxCells (matrix, boxIndex) {
+        const startRowIndex = Math.floor(boxIndex / 3) * 3;
+        const startColIndex = boxIndex % 3 * 3;
+        const result = [];
+        for (let cellIndex = 0; cellIndex < 9; cellIndex++) {
+            const rowIndex = startRowIndex + Math.floor(cellIndex / 3);
+            const colIndex = startColIndex + cellIndex % 3;
+            result.push(matrix[rowIndex][colIndex]);
+        }
+        return result;
+    }
 };
 
 // 工具集
@@ -68,6 +99,6 @@ module.exports  = class Toolkit {
      * 宫坐标系工具
      */
     static get box () {
-        return boxToolit;
+        return boxTookit;
     }
 }
